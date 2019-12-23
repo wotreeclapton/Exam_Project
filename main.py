37fe6c288d7dc4dae2b54cb8bdd5618a8f3a0f18
wotreeclapton <wotreeclapton@hotmail.com>
2019-12-23 16:42:47 +0700

    settings file updates

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 EXAM APPLICATION LAUNCHER developed by Mr Steven J walden
     Nov. 2019
     SAMROIYOD, PRACHUAP KIRI KHAN, THAILAND
-[See license at end of file]
-
+[See License.txt file]
 '''
 '''
-Raise error report on file not saving to the netwok location
+Raise error reports
+	setttings.csv not found
+	student details file not found -done
+	student photo not found - done
+	exam question csv not found -done
+	network location not found - done
+	results xlsx written to - done
+	video error
 Save results to diffent class tabs
 smallest screen is 1280x1024
 for logging use  exc_info=1 in error string to print exception info
@@ -15,7 +21,7 @@ or use exception
 '''
 
 __author__ = 'Mr Steven J Walden'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 import os
 import sys
@@ -58,7 +64,24 @@ class App(QtWidgets.QWidget):
 		self.open_login_window()
 
 	def load_data(self):
-		self.class_list = ['Choose your class','M1/1','M2/1','M3/1']
+		#Load network location for classes, exam files and results
+		with open('resources/load_location.txt') as file_setup:
+			self.network_location = file_setup.read()
+			print(str(self.network_location))
+		#load classes
+		self.class_list = []
+		with change_dir(r'//ep02/Public/Steve', self.logger):####error
+			try:
+				with open('Settings.csv','r') as csv_file:
+					csv_reader = csv.DictReader(csv_file)
+
+					for line in csv_reader:
+						self.class_list.append(line['classes'])
+			except FileNotFoundError:
+				self.logger.error(" Cannot load the settings file!")
+				os.chdir('..')
+				self.message_boxes(msg='FileNotFoundError', msg_type=2)
+
 		self.string_convert = {'A':1,'B':2,'C':3,'D':4}
 		self.student_names, self.student_nicknames, self.student_passwords, = [], [], []
 		self.exam_questions, self.exam_AnswerA, self.exam_AnswerB, self.exam_AnswerC, self.exam_AnswerD, self.exam_Rightanswer, self.exam_photoquestion = [],[],[],[],[],[],[]
@@ -80,14 +103,19 @@ class App(QtWidgets.QWidget):
 		self.student_passwords.clear()
 
 		with change_dir('resources', self.logger):
-			#with open('Student_Details_CSV_M' + str(clas) + '-1.csv','r') as csv_file:
-			with open('Student_Details_CSV_M{}-1.csv'.format(str(clas)),'r') as csv_file:
-				csv_reader = csv.DictReader(csv_file)
+			self.path = 'Student_Details_CSV_M{}-1.csv'.format(str(clas))
+			try:
+				with open(self.path,'r') as csv_file:
+					csv_reader = csv.DictReader(csv_file)
 
-				for line in csv_reader:
-					self.student_names.append(line['Name'])
-					self.student_nicknames.append(line['Nicknames'])
-					self.student_passwords.append(line['Passwords'])
+					for line in csv_reader:
+						self.student_names.append(line['Name'])
+						self.student_nicknames.append(line['Nicknames'])
+						self.student_passwords.append(line['Passwords'])
+			except FileNotFoundError:
+				self.logger.error(" Can not find the file {}".format(self.path))
+				os.chdir('..')
+				self.message_boxes(msg='FileNotFoundError', msg_type=2)
 
 	def open_login_window(self):
 		self.login_gui = Ui_ExamLogin()
@@ -155,12 +183,13 @@ class App(QtWidgets.QWidget):
 
 		with change_dir('img', self.logger):
 			if st > 0:
-				self.path_test = path.join('M' + str(self.year_chosen) + '-1', str(st) + '.png')
-				if path.exists(self.path_test):
-					self.login_gui.StudentPhoto.setPixmap(QtGui.QPixmap(path.join(self.path_test)))
+				self.photo_path = 'M{}-1/{}.png'.format(str(self.year_chosen), str(st))
+				if path.exists(self.photo_path):	
+					self.login_gui.StudentPhoto.setPixmap(QtGui.QPixmap(self.photo_path))
 				else:
+					self.logger.error(" {}'s photo {}.png is missing in M{}-1 folder".format(self.student_nicknames[st], str(st), str(st)))
 					self.login_gui.StudentPhoto.setPixmap(QtGui.QPixmap('blank_girl.png'))
-
+					
 				self.login_gui.StudentNumber.setText(str(st))
 				self.login_gui.StudentNickname.setText(self.student_nicknames[st])
 			else:
@@ -201,7 +230,6 @@ class App(QtWidgets.QWidget):
 		self.read_exam_questions_csv()
 
 		#set the text etc
-		#self.exam_gui.setWindowTitle(self.exam_questions[0] + " " + self.exam_AnswerA[0] + " Questions")
 		self.exam_gui.setWindowTitle("{} {} Questions".format(self.exam_questions[0], self.exam_AnswerA[0]))
 		self.exam_gui.ExamTitle.setText(self.exam_questions[0]+ "\n" + self.exam_AnswerA[0])
 		self.exam_gui.StartTime.setText(self.start_time.strftime("%H:%M:%S"))
@@ -211,8 +239,11 @@ class App(QtWidgets.QWidget):
 		self.exam_gui.StudentNicknameLabel.setText(self.student_nicknames[self.student_number])
 		self.exam_gui.StudentNameLabel.setText(self.student_names[self.student_number])
 		with change_dir('img', self.logger):
-			#self.exam_gui.StudentPhotoLabel.setPixmap(QtGui.QPixmap(path.join('M' + str(self.year_chosen) + '-1', str(self.student_number) + '.png')))
-			self.exam_gui.StudentPhotoLabel.setPixmap(QtGui.QPixmap(path.join('M{}-1'.format(str(self.year_chosen)), str(self.student_number) + '.png')))
+			if path.exists(self.photo_path):	
+				self.exam_gui.StudentPhotoLabel.setPixmap(QtGui.QPixmap(self.photo_path))
+			else:
+				self.exam_gui.StudentPhotoLabel.setPixmap(QtGui.QPixmap('blank_girl.png'))
+
 		#self.exam_gui.tabWidget.setCurrentIndex(0)
 
 		self.exam_gui.TimeLeftProgressBar.setMaximum(self.allowed_time)
@@ -243,9 +274,9 @@ class App(QtWidgets.QWidget):
 		with change_dir('resources', self.logger):
 			#open the correct csv file for each exam self/class name
 			#Test to see if the file exsists if not raise error and close program
-			self.path_test = ('{}_Exam_Questions.csv'.format(self.class_name[:2]))
-			if path.exists(self.path_test):
-				with open('{}_Exam_Questions.csv'.format(self.class_name[:2]),'r') as csv_file:
+			self.path = '{}_Exam_Questions.csv'.format(self.class_name[:2])
+			try:
+				with open(self.path,'r') as csv_file:
 					csv_reader = csv.DictReader(csv_file)
 
 					for line in csv_reader:
@@ -256,14 +287,10 @@ class App(QtWidgets.QWidget):
 						self.exam_AnswerD.append(line['AnswerD'])
 						self.exam_Rightanswer.append(line['Rightanswer'])
 						self.exam_photoquestion.append(line['Photoquestion'])
-			else:
-				try:
-					raise Exception('File not found!')
-				except:
-					self.logger.error(" Can not find the file {}".format(self.path_test))
-					os.chdir('..')
-					self.message_boxes(msg='FlleNotFoundError', msg_type=2)
-					sys.exit(app.exec_())
+			except FileNotFoundError:
+				self.logger.error(" Can not find the file {}".format(self.path))
+				os.chdir('..')
+				self.message_boxes(msg='FileNotFoundError', msg_type=2)
 
 	def counters(self):
 		self.scroll_thread = ScrollThread(parent=None, alloted_time=self.allowed_time)
@@ -296,7 +323,6 @@ class App(QtWidgets.QWidget):
 			self.msgbox.setIcon(QMessageBox.Critical)
 			self.msgbox.setStandardButtons(QMessageBox.Ok)
 		if msg_type == 1:
-			#self.msgbox.setText('Your score is ' + str(self.correct_answers) + '/' + str(len(self.exam_questions)-1))
 			self.msgbox.setText('Your score is {}/{}'.format(str(self.correct_answers), str(len(self.exam_questions)-1)))
 			self.msgbox.setIcon(QMessageBox.Information)
 			self.msgbox.setStandardButtons(QMessageBox.Ok)
@@ -309,10 +335,13 @@ class App(QtWidgets.QWidget):
 		if self.msgbox.exec() == QMessageBox.Ok:
 			self.allowed_time = 20 * 600
 			self.question_number = 1
-			self.scroll_thread.is_running = False
-			self.time_thread.is_running = False
-			self.exam_gui.close()
-			self.open_login_window()
+			if msg_type != 2:
+				self.scroll_thread.is_running = False
+				self.time_thread.is_running = False
+				self.exam_gui.close()
+				self.open_login_window()
+			else:
+				sys.exit(app.exec_())			
 
 	def save_results(self):
 		self.time_finished = datetime.datetime.today()
@@ -320,14 +349,14 @@ class App(QtWidgets.QWidget):
 		self.result_list = [self.student_number, self.student_names[self.student_number], self.student_nicknames[self.student_number], self.correct_answers, self.start_time.strftime("%d/%m/%Y"), self.start_time.strftime("%H:%M:%S"), self.time_finished.strftime("%H:%M:%S")]
 		#Check for exsisting excel file
 		self.results_filename = "{} {} results.xlsx".format(self.exam_questions[0], self.exam_AnswerA[0])
-		with change_dir('resources', self.logger): #r'\\ep02\Public\Steve' use format for network location
+		with change_dir(r'//ep02/Public/Steve', self.logger): #r'\\ep02\Public\Steve' use format for network location
 			try:
 				self.results_wb = load_workbook(filename = self.results_filename) #opening the file
 				self.write_to_result_wb()
 			except FileNotFoundError:
 				self.results_wb = Workbook() #create the workbook then write to workbook method and save
 				self.write_to_result_wb()
-				self.logger.error(" Created: {}".format(self.results_filename))
+				self.logger.error(" Created: {} in {}".format(self.results_filename, os.getcwd()))
 
 	def write_to_result_wb(self):
 		self.header_list = ['Number','Name','Nickname','Score','Day Taken','Time Started','Time Finished']
@@ -418,24 +447,3 @@ if __name__ == '__main__':
 
 sys.exit(app.exec_())
 
-
-
-# Copyright (c) 2019 Steven Walden
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
